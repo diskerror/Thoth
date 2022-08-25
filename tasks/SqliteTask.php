2a9a5e34f9d20803a92726c23ab240da7f01c41e
Reid Woodbury Jr <reid@rawsound.com>
2022-08-25 16:05:59 -0700

    Dump SQLite file.

---
+++ tasks/SqliteTask.php
@@ -0,0 +1,38 @@
+<?php
+
+use Service\StdIo;
+
+class SqliteTask extends TaskMaster
+{
+	function dumpAction(string $fName)
+	{
+		if (!file_exists($fName)) {
+			throw new RuntimeException('SQLite file does not exist');
+		}
+
+		$db = new Sqlite3($fName, SQLITE3_OPEN_READONLY);
+		$db->exec('PRAGMA journal_mode = wal;');
+		StdIo::outln('SQLite version: ' . $db->version()['versionString']);
+
+		$tables = $db->query('PRAGMA table_list');
+		if ($tables === false) {
+			StdIo::outln('PRAGMA table_list returned false');
+			return;
+		}
+
+		while ($table = $tables->fetchArray(SQLITE3_ASSOC)) {
+			$tDump = $db->query('SELECT * FROM ' . $table['name']);
+			if ($tDump === false) {
+				StdIo::outln($table['name'] . ' query returned false');
+				StdIo::outln();
+				continue;
+			}
+
+			StdIo::outln($table['name'] . ':');
+			while ($td = $tDump->fetchArray(SQLITE3_ASSOC)) {
+				StdIo::jsonOut($td);
+			}
+			StdIo::outln();
+		}
+	}
+}
