59f0d76996d3eb293e803411ade400fdc7e253b6
Reid Woodbury Jr <reid@rawsound.com>
2022-08-25 16:06:41 -0700

    Added XML plist handling.

--- tasks/XmlTask.php
+++ tasks/XmlTask.php
@@ -1,17 +1,27 @@
 <?php
 
+use CFPropertyList\CFPropertyList;
 use Service\StdIo;
 use Service\XmlParser;
 
 class XmlTask extends TaskMaster
 {
-	function toJsonAction(string $fileName)
+	function XmlToJsonAction(string $fileName)
 	{
 		mb_internal_encoding('UTF-8');
 //		ini_set('memory_limit', 100000000000);
 
-		$xmlText = file_get_contents($fileName);
+		$xmlText   = file_get_contents($fileName);
 		$xmlObject = new XmlParser($xmlText);
 		StdIo::phpOut($xmlObject->array);
+	}
+
+	function PListToJsonAction(string $fileName)
+	{
+		mb_internal_encoding('UTF-8');
+
+		$plist = new CFPropertyList($fileName);
+
+		StdIo::phpOut($plist->toArray());
 	}
 }
